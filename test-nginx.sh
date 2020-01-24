2a52b93e012748066fac726c2ee3647f5c08f9f0
Yoshihiko Kanaumi <yoshihiko.kanaumi@gmail.com>
2020-01-24 13:04:07 +0900

    Test script for nginx on k8s

---
+++ test-nginx.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+ssh root@master1-ha kubectl get svc -o jsonpath='{.items[*].status.loadBalancer.ingress[0].ip}' > external_ip
+
+#echo "The test of the reachability by cURL"
+#echo "The number of external ip is "$#
+
+for i in `cat ./external_ip`
+do
+	echo "The external ip for LoadBalancer ip is "$i
+	for j in `seq 10`
+	do
+		curl -s $i
+	        sleep 1
+	done
+done
+
+
+#for i in $*
+#do
+#	echo "The external ip is "$i
+#	for j in `seq 10`
+#	do
+#		curl -s $i
+#		sleep 1
+#	done
+#done
+
